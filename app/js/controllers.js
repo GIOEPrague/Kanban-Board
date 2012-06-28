c17eb4b937c59cd80fcef0c4d0e9990d04f3bd84
Ladislav Vondracek <lawondyss@gmail.com>
2012-06-28 05:44:51 +0200

    app/column-form.html added app/js/controllers.js changed
    column-form.html
    HTML template for column form.
    
    controllers.js
    added object ColumnFormCtrl
    added values column, errMessage, patternName, patternPozitiveNumber, patternColor
    added methods submit, add, update
    Controller for column form.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,10 +3,79 @@
 /* Controllers */
 
 
-function MyCtrl1() {}
-MyCtrl1.$inject = [];
+/**
+ * @desc Handle form for columns
+ * @author lawondyss@gmail.com (Ladislav Vondracek)
+ * @version 0.1
+ */
+function ColumnFormCtrl($scope)
+{
+	// data for form
+	$scope.column = {'id':8, 'name':'první', 'order':1, 'limit':10, 'color':'#336699'};
+
+	// message for submit with errors
+	$scope.errMessage = '';
+
+	// patterns for validation
+	$scope.patternName = /^.{1,20}$/;
+	$scope.patternPozitiveNumber = /^[0-9]+$/;
+	$scope.patternColor = /^#[0-9A-Fa-f]{6}$/;
 
 
-function MyCtrl2() {
+	/**
+	 * @desc Submitted form
+	 */
+	$scope.submit = function()
+	{
+		// submitted with errors
+		if(this.columnForm.$invalid)
+		{
+			// error message
+			$scope.errMessage = 'Invalid values in form.'
+		}
+		else
+		{
+			// cleared error message
+			$scope.errMessage = '';
+
+			// values to save
+			var data = {
+				'name':this.column.name,
+				'order':this.column.order,
+				'limit':this.column.limit,
+				'color':this.column.color
+			};
+
+			// add or update
+			if($scope.column.id)
+			{
+				$scope.update($scope.column.id, data);
+			}
+			else
+			{
+				$scope.add(data);
+			}
+		}
+	}
+
+
+	/**
+	 * @desc Add new column
+	 * @param JSON
+	 */
+	$scope.add = function(data)
+	{
+		$scope.board.addColumn(data.name, data.order, data.limit, data.color);
+	}
+
+
+	/**
+	 * @desc Update column
+	 * @param int
+	 * @param JSON
+	 */
+	$scope.update = function(id, data)
+	{
+		$scope.board.updateColumn(id, data.name, data.order, data.limit, data.color);
+	}
 }
-MyCtrl2.$inject = [];
